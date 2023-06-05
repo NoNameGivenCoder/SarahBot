e17e85429943813965c374de55ac7543455c7eaf
NoNameGiven <114644239+NoNameGivenCoder@users.noreply.github.com>
2023-06-05 09:43:30 -0700

    added a few more values to serverstats

--- commands/utility/serverstats.js
+++ commands/utility/serverstats.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { embedColor } = require('../../config.json')
 
 module.exports = {
     data : new SlashCommandBuilder() 
@@ -12,8 +13,11 @@ module.exports = {
                 {name : 'Server Name', value : `${interaction.guild.name}`, inline : true},
                 {name : 'Server Description', value : `${interaction.guild.description}`, inline : true},
                 {name : 'Server Member Count', value : `${interaction.guild.memberCount}`, inline : true},
+                {name : 'Created On', value : `${interaction.guild.createdAt.getUTCMonth()}/${interaction.guild.createdAt.getUTCDate()}/${interaction.guild.createdAt.getUTCFullYear()}`, inline : true},
+                {name : 'Verfied?', value : `${interaction.guild.verified}`, inline : true},
             )
-            .setThumbnail(interaction.guild.iconURL());
+            .setThumbnail(interaction.guild.iconURL())
+            .setColor(embedColor);
 
         await interaction.reply({ embeds : [embedStats] });
     },
