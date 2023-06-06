c43dde681ddf062a3543807909a1739b3b52d25c
NoNameGiven <114644239+NoNameGivenCoder@users.noreply.github.com>
2023-06-05 21:36:59 -0700

    david is not gay

--- commands/utility/whatistvii.js
+++ commands/utility/whatistvii.js
@@ -8,6 +8,5 @@ module.exports = {
     async execute (interaction) {
         newEmbed = new EmbedBuilder().setColor(embedColor).setTitle("What is TVii?").setDescription('Nintendo TVii is a service that allows users to find and watch television shows using their Wii U console, created as a collaboration between Nintendo and i.TV and launching with a system update in December 2012, after the release of the console in November. \n \n Users connect to their cable providers or online streaming services such as Netflix, Hulu Plus, or Amazon Instant Video and search for shows that they want to watch that are available to watch on either the television screen or the Wii U GamePad.')
         await interaction.reply({embeds : [newEmbed]});
-        //WRITE LATER SINCE DAVID IS GAY
     },
 };
