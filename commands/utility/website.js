44563f4dd4e4ffcb43a46cf485351b88f0c7c88f
NoNameGiven <114644239+NoNameGivenCoder@users.noreply.github.com>
2023-06-05 21:36:05 -0700

    added more embeds

--- commands/utility/website.js
+++ commands/utility/website.js
@@ -1,11 +1,12 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { website } = require('../../config.json')
+const { SlashCommandBuilder, EmbedBuilder, Embed } = require('discord.js');
+const { website, embedColor } = require('../../config.json')
 
 module.exports = {
     data : new SlashCommandBuilder() 
         .setName('website')
         .setDescription('The server website'),
     async execute (interaction) {
-        await interaction.reply(website);
+        newEmbed = new EmbedBuilder().setColor(embedColor).setTitle("Server Website").setDescription(website)
+        await interaction.reply({embeds : [newEmbed]});
     },
 };
--- commands/utility/whatistvii.js
+++ commands/utility/whatistvii.js
@@ -1,11 +1,13 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { embedColor } = require('../../config.json')
 
 module.exports = {
     data : new SlashCommandBuilder() 
         .setName('whatistvii')
         .setDescription('What is TVii?'),
     async execute (interaction) {
-        await interaction.reply('Nintendo TVii is a service that allows users to find and watch television shows using their Wii U console, created as a collaboration between Nintendo and i.TV and launching with a system update in December 2012, after the release of the console in November. Users connect to their cable providers or online streaming services such as Netflix, Hulu Plus, or Amazon Instant Video and search for shows that they want to watch that are available to watch on either the television screen or the Wii U GamePad.');
+        newEmbed = new EmbedBuilder().setColor(embedColor).setTitle("What is TVii?").setDescription('Nintendo TVii is a service that allows users to find and watch television shows using their Wii U console, created as a collaboration between Nintendo and i.TV and launching with a system update in December 2012, after the release of the console in November. \n \n Users connect to their cable providers or online streaming services such as Netflix, Hulu Plus, or Amazon Instant Video and search for shows that they want to watch that are available to watch on either the television screen or the Wii U GamePad.')
+        await interaction.reply({embeds : [newEmbed]});
         //WRITE LATER SINCE DAVID IS GAY
     },
 };
